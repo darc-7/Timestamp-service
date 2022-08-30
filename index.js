00138337dcf3999892e7228414c10a99804ffe1c
Daniel Ramos <darc7zed@gmail.com>
2022-08-30 11:45:26 -0400

    unused code eliminated

--- index.js
+++ index.js
@@ -33,22 +33,17 @@ app.get("/api/:date?", function (req, res) {
     res.json({unix: nowDate.getTime(), utc: nowDate.toUTCString()})
   }
   else if (reqDate.toString().includes('-') || reqDate.toString().includes(' ')){
-      reqDate = new Date(req.params.date);
-      res.json({unix: reqDate.getTime(), utc: reqDate.toUTCString()}); 
+    reqDate = new Date(req.params.date);
+    console.log(reqDate);
+    if(reqDate.toUTCString() === 'Invalid Date'){
+      res.json({ error : "Invalid Date" });
+    }
+    res.json({unix: reqDate.getTime(), utc: reqDate.toUTCString()}); 
   }
   else{
       res.json({ error : "Invalid Date" });
   }
-  /*else{
-    if (reqDate.toString().match(regEx) === null) {
-      res.json({ error : "Invalid Date" });
-    }
-    else{
-      reqDate = new Date(req.params.date);
-      res.json({unix: reqDate.getTime(), utc: reqDate.toUTCString()});  
-    }
-  }*/
-  console.log(reqDate);
+  //console.log(reqDate);
 });
 
 // listen for requests :)
